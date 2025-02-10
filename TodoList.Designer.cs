e0758d445858c422f56ecaf0af1e03735a88fcf0
leilamo <leilam@aiutrix.com>
2025-02-10 15:27:53 +0100

    Initial commit - Upload TodoApp

---
+++ TodoList.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace TodoApp
+{
+    partial class TodoList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lstTasks = new System.Windows.Forms.ListBox();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnClearSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTask
+            // 
+            this.txtTask.Location = new System.Drawing.Point(62, 37);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(502, 22);
+            this.txtTask.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(581, 36);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(168, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add Task";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lstTasks
+            // 
+            this.lstTasks.FormattingEnabled = true;
+            this.lstTasks.ItemHeight = 16;
+            this.lstTasks.Location = new System.Drawing.Point(62, 110);
+            this.lstTasks.Name = "lstTasks";
+            this.lstTasks.Size = new System.Drawing.Size(687, 260);
+            this.lstTasks.TabIndex = 2;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(62, 397);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(168, 23);
+            this.btnEdit.TabIndex = 3;
+            this.btnEdit.Text = "Edit Task";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(270, 397);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(168, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete Task";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(62, 65);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(502, 22);
+            this.txtSearch.TabIndex = 5;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // btnClearSearch
+            // 
+            this.btnClearSearch.Location = new System.Drawing.Point(581, 65);
+            this.btnClearSearch.Name = "btnClearSearch";
+            this.btnClearSearch.Size = new System.Drawing.Size(168, 23);
+            this.btnClearSearch.TabIndex = 6;
+            this.btnClearSearch.Text = "Clear";
+            this.btnClearSearch.UseVisualStyleBackColor = true;
+            this.btnClearSearch.Click += new System.EventHandler(this.btnClearSearch_Click);
+            // 
+            // TodoList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClearSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.lstTasks);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtTask);
+            this.Name = "TodoList";
+            this.Text = "TodoList";
+            this.Load += new System.EventHandler(this.TodoList_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lstTasks;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnClearSearch;
+    }
+}
+
---
+++ TodoList.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TodoApp
+{
+    public partial class TodoList : Form
+    {
+        private List<string> allTasks = new List<string>();  
+
+        public TodoList()
+        {
+            InitializeComponent();
+        }
+
+        private void TodoList_Load(object sender, EventArgs e)
+        {
+            LoadTasks();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex != -1)
+            {
+                string newTask = PromptDialog("Edit Task:", "Edit Task");
+                if (!string.IsNullOrWhiteSpace(newTask))
+                {
+                    lstTasks.Items[lstTasks.SelectedIndex] = newTask;
+                    SaveTasks();
+                }
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtTask.Text))
+            {
+                lstTasks.Items.Add(txtTask.Text);
+                txtTask.Clear();
+                SaveTasks();  // ذخیره لیست تسک‌ها
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex != -1)
+            {
+                lstTasks.Items.RemoveAt(lstTasks.SelectedIndex);
+                SaveTasks();
+            }
+        }
+
+        private string PromptDialog(string text, string caption)
+        {
+            Form prompt = new Form()
+            {
+                Width = 300,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+
+            Label lblText = new Label() { Left = 10, Top = 20, Text = text };
+            TextBox txtInput = new TextBox() { Left = 10, Top = 50, Width = 260 };
+            Button btnOk = new Button() { Text = "OK", Left = 100, Width = 100, Top = 80 };
+
+            btnOk.Click += (sender, e) => { prompt.DialogResult = DialogResult.OK; prompt.Close(); };
+
+            prompt.Controls.Add(lblText);
+            prompt.Controls.Add(txtInput);
+            prompt.Controls.Add(btnOk);
+            prompt.AcceptButton = btnOk;
+
+            return prompt.ShowDialog() == DialogResult.OK ? txtInput.Text : "";
+        }
+
+        private void SaveTasks()
+        {
+            allTasks.Clear();
+            foreach (var item in lstTasks.Items)
+            {
+                allTasks.Add(item.ToString());
+            }
+
+            string json = JsonConvert.SerializeObject(allTasks, Formatting.Indented);
+            File.WriteAllText("tasks.json", json);
+        }
+
+        private void LoadTasks()
+        {
+            if (File.Exists("tasks.json"))
+            {
+                string json = File.ReadAllText("tasks.json");
+                allTasks = JsonConvert.DeserializeObject<List<string>>(json) ?? new List<string>();
+
+                lstTasks.Items.Clear();
+                foreach (var task in allTasks)
+                {
+                    lstTasks.Items.Add(task);
+                }
+            }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            string query = txtSearch.Text.ToLower();
+            lstTasks.Items.Clear();
+
+            foreach (var task in allTasks)
+            {
+                if (task.ToLower().Contains(query))
+                {
+                    lstTasks.Items.Add(task);
+                }
+            }
+        }
+
+        private void btnClearSearch_Click(object sender, EventArgs e)
+        {
+            txtSearch.Clear();
+            lstTasks.Items.Clear();
+
+            foreach (var task in allTasks)
+            {
+                lstTasks.Items.Add(task);
+            }
+        }
+    }
+}
